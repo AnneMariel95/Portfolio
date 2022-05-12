c50f6cd5cfc873f3ddf6ba476267239c6ec015cd
AnneMariel95 <a.mariel.recio@gmail.com>
2022-05-12 20:37:46 +0800

    First commit

---
+++ pages/blog.js
@@ -0,0 +1,11 @@
+const Blog = () => {
+  return (
+    <div>
+      <h1>Blog</h1>
+      <p>Est irure culpa mollit enim velit.</p>
+      <p>Ad velit pariatur aute velit officia.</p>
+    </div>
+  );
+};
+
+export default Blog;
